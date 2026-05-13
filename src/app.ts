119eaaebe9a575f6ba6257553242053f3ce774a1
chzwendel <christianwendell@hotmail.com>
2026-05-12 21:52:42 -0300

    comit dia 12/05/2026

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,8 @@ import { AmbienteRepository } from "./repositories/AmbienteRepository";
 
 export const app = express();
 
-Con()
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Servidorr rodando em http://localhost:3000");
+app.listen(3002, () => {
+  console.log("Servidorr rodando em http://localhost:3002");
 });
